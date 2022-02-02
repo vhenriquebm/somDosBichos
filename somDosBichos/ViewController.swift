ec2d5521a09031190909a86d10aa7c6abec2cbaa
Vitor Henrique <vhenriquebm@gmail.com>
2022-02-02 18:40:36 -0300

    Primeira versão com métodos de som importados do foundations, stack views e image views

--- somDosBichos/ViewController.swift
+++ somDosBichos/ViewController.swift
@@ -6,8 +6,11 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewControllerBichos: UIViewController {
+    
+    var player = AVAudioPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,5 +18,65 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func cao(_ sender: Any) {
+        self.executarSom(nomeSom: "cao")
+        
+    }
+    
+    
+    @IBAction func gato(_ sender: Any) {
+        self.executarSom(nomeSom: "gato")
+
+        
+    }
+    
+    
+    @IBAction func leao(_ sender: Any) {
+        self.executarSom(nomeSom: "leao")
+
+    }
+    
+    
+    @IBAction func macaco(_ sender: Any) {
+        self.executarSom(nomeSom: "macaco")
+
+    }
+    
+    
+    @IBAction func ovelha(_ sender: Any) {
+        self.executarSom(nomeSom: "ovelha")
+
+    }
+    
+    
+    @IBAction func vaca(_ sender: Any) {
+        self.executarSom(nomeSom: "vaca")
+
+    }
+    
+    
+    func executarSom (nomeSom:String){
+        
+        
+        if let path = Bundle.main.path(forResource: nomeSom, ofType: "mp3") {
+        let url = URL(fileURLWithPath: path)
+            
+        do {
+        player = try AVAudioPlayer(contentsOf: url)
+        player.prepareToPlay()
+            player.play()
+            
+        }catch {
+                
+                print ("Erro ao executar o audio")
+        }
+        
+        
+    }
+    
 }
+
+}
+
+
 
